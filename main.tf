e8e4318ce56dd301636503cbd759072b84cc60dc
estelaacastillo <estela.castillo@student.ie.edu>
2024-05-22 14:17:16 +0200

    Iniciar commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "my-rds-subnet-group"
+  subnet_ids = ["subnet-0b81b078351dc667a", "subnet-03b57312e9b648f24"]  # Replace with your subnet IDs in different AZs
+
+  tags = {
+    Name = "My RDS subnet group"
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  db_name              = "esteladb"
+  username             = "admin"
+  password             = "Monterroso77777!"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.default.name
+
+  tags = {
+    Name = "PrimaryRDSInstance"
+  }
+}
+
+output "aws_rds_endpoint" {
+  value = aws_db_instance.default.endpoint
+}
+
