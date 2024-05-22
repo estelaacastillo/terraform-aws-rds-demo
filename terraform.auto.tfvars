cdba2c160fa4d8d22e1eb05c4ad4a1c37a5b2806
estelaacastillo <estela.castillo@student.ie.edu>
2024-05-22 14:31:44 +0200

    Commit final

---
+++ terraform.auto.tfvars
@@ -0,0 +1,2 @@
+aws_region = "eu-west-3"
+db_name              = "esteladb"
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  type = string
+  description = "Default aws region to deploy into"
+  default = "us-east-1"
+  }
+
+variable "db_name" {
+  type = string
+  description = "Default aws region to deploy into"
+  }
